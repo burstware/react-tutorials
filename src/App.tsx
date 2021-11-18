e28006c906c6145240d1c642cba6ecf14fb97a3d
Jordan Baczuk <jordan.baczuk@gmail.com>
2021-11-17 19:57:37 -0700

    add ga

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,46 @@
-import React, { useState } from "react";
-import "./App.css";
-import ReactDnd from "./DragAndDrop/DragAndDrop";
-import UsePrevious from "./UsePrevious";
+import React, { useState } from 'react'
+import './App.css'
+import ReactDnd from './DragAndDrop/DragAndDrop'
+import UsePrevious from './UsePrevious'
 
 function App() {
-  const [selection, setSelection] = useState("");
+  const [selection, setSelection] = useState('')
   return (
     <div
       style={{
-        margin: "1rem",
-        display: "flex",
-        flexDirection: "column",
-        gap: "0.5rem",
+        margin: '1rem',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '0.5rem'
       }}
     >
-      <h1 style={{ cursor: "pointer" }} onClick={() => setSelection("")}>
+      <h1 style={{ cursor: 'pointer' }} onClick={() => setSelection('')}>
         Tutorials
       </h1>
-      <ul style={{ display: "flex", flexDirection: "column", gap: "0.5rem" }}>
+      <ul style={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
         <li
           style={{
-            cursor: "pointer",
-            color: selection === "react-dnd" ? "red" : "blue",
+            cursor: 'pointer',
+            color: selection === 'react-dnd' ? 'red' : 'blue'
           }}
-          onClick={() => setSelection("react-dnd")}
+          onClick={() => setSelection('react-dnd')}
         >
           react-dnd
         </li>
         <li
           style={{
-            cursor: "pointer",
-            color: selection === "use-previous" ? "red" : "blue",
+            cursor: 'pointer',
+            color: selection === 'use-previous' ? 'red' : 'blue'
           }}
-          onClick={() => setSelection("use-previous")}
+          onClick={() => setSelection('use-previous')}
         >
           use-previous
         </li>
       </ul>
-      {selection === "react-dnd" && <ReactDnd />}
-      {selection === "use-previous" && <UsePrevious />}
+      {selection === 'react-dnd' && <ReactDnd />}
+      {selection === 'use-previous' && <UsePrevious />}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
